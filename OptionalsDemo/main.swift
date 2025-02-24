e189db73c9f7145c3b42faa5e79f3006c3a83c44
Carlo Contardi <c.contardi@spacecoding.it>
2025-02-24 16:48:27 +0100

    [Feature] add: Check if nil value method

--- OptionalsDemo/main.swift
+++ OptionalsDemo/main.swift
@@ -11,12 +11,25 @@ myOptional = "Carlo"
 // Il seguente codice non funziona perchè text si aspetta una String e non un Optional
 // --> let text: String = myOptional
 
-// MARK:  1. Force Unwrapping - Il metodo meno sicuro per lavorare con gli Optionals (quello da evitare) 
+// MARK:  1. Force Unwrapping - Il metodo meno sicuro per lavorare con gli Optionals (quello da evitare)
 
 let text : String = myOptional!
 
 //   se myOptional = nil  XCode non darebbe errore
 //   ma in runtime il codice si blocca
+
+// MARK: 2. Check if nil value - metodo prolisso
+
+let myOptional2: String?
+
+myOptional2 = nil
+
+if myOptional2 != nil{
+    let text : String = myOptional2!
+    
+}else{
+    print("myOptional2 was found to be nil. ")
+}
 
 
 
