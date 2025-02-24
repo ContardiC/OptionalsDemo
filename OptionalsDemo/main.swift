3b8d1c8aab5234aed92239a25598ac50db5d53a6
Carlo Contardi <c.contardi@spacecoding.it>
2025-02-24 16:43:20 +0100

    [Feature] add: Force Unwrapping method

--- OptionalsDemo/main.swift
+++ OptionalsDemo/main.swift
@@ -5,7 +5,20 @@
 //  Created by Carlo Contardi on 24/02/25.
 //
 
-import Foundation
+let myOptional: String?
 
-print("Hello, World!")
+myOptional = "Carlo"
+// Il seguente codice non funziona perchè text si aspetta una String e non un Optional
+// --> let text: String = myOptional
+
+// MARK:  1. Force Unwrapping - Il metodo meno sicuro per lavorare con gli Optionals (quello da evitare) 
+
+let text : String = myOptional!
+
+//   se myOptional = nil  XCode non darebbe errore
+//   ma in runtime il codice si blocca
+
+
+
+
 
